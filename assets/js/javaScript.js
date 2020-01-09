6f571b6369266e08ea548be8a38ef91eea0e2316
Brianna McCray <brianna.mccray@outlook.com>
2020-01-09 09:56:13 -0600

    Animation to Spotify, button click functions

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -109,6 +109,13 @@ var spotifyFrameEl = $("#spotifyFrame");
 var spotifyDivEl = $("#spotifyDiv");
 var spotifyUrl = "https://open.spotify.com/embed/playlist/";
 
+var userNum;
+var monthEl = $("#month").val();
+var yearEl = $("#year").val();
+
+console.log(monthEl);
+console.log(yearEl);
+
 var userSign = {
 
     aries : "37i9dQZF1DX2DC3Q7JOmYe",
@@ -124,21 +131,87 @@ var userSign = {
     aquarius: "37i9dQZF1DX7F9VDRJOFhw",
     pisces: "37i9dQZF1DWX0EDWtabVRv"
 
-}
-
-//logging results
-console.log(spotifyUrl + userSign.aries);
-console.log(spotifyUrl + userSign.taurus);
+};
 
 //button click functions
+$("#aries").on("click", function(event) {
+    console.log(spotifyUrl + userSign.aries);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN ARIES IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+    //adding attribute of URL to iframe tag in index
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.aries);
+    //appending the element to the div holding iframe
+    spotifyDivEl.append(spotifyFrameEl);
+
+}); 
+
+$("#taurus").on("click", function(event) {
+    console.log(spotifyUrl + userSign.taurus);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN TAURUS IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.taurus);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
 $("#gemini").on("click", function(event) {
     console.log(spotifyUrl + userSign.gemini);
     event.preventDefault();
     console.log("DEBUG: THIS WILL SHOW WHEN GEMINI IS CLICKED");
-    console.log("EXAMPLE OF BUTTON");    
+    console.log("EXAMPLE OF SPAN");    
+
     
    
     spotifyFrameEl.attr("src", spotifyUrl + userSign.gemini);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
+$("#cancer").on("click", function(event) {
+    console.log(spotifyUrl + userSign.cancer);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN CANCER IS CLICKED");  
+    console.log("EXAMPLE OF PTAG");    
+
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.cancer);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
+$("#leo").on("click", function(event) {
+    console.log(spotifyUrl + userSign.leo);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN LEO IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.leo);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
+$("#virgo").on("click", function(event) {
+    console.log(spotifyUrl + userSign.virgo);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN VIRGO IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.virgo);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
+$("#libra").on("click", function(event) {
+    console.log(spotifyUrl + userSign.libra);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN LIBRA IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.libra);
     spotifyDivEl.append(spotifyFrameEl);
 
 });
@@ -146,26 +219,83 @@ $("#scorpio").on("click", function(event) {
     console.log(spotifyUrl + userSign.scorpio);
     event.preventDefault();
     console.log("DEBUG: THIS WILL SHOW WHEN SCORPIO IS CLICKED");
-    console.log("EXAMPLE OF SPAN");    
-
+    console.log("EXAMPLE OF BUTTON");    
     
    
     spotifyFrameEl.attr("src", spotifyUrl + userSign.scorpio);
     spotifyDivEl.append(spotifyFrameEl);
 
 });
-$("#virgo").on("click", function(event) {
-    console.log(spotifyUrl + userSign.virgo);
+$("#sagittarius").on("click", function(event) {
+    console.log(spotifyUrl + userSign.sagittarius);
     event.preventDefault();
-    console.log("DEBUG: THIS WILL SHOW WHEN VIRGO IS CLICKED");  
-    console.log("EXAMPLE OF PTAG");    
-
+    console.log("DEBUG: THIS WILL SHOW WHEN SAGITTARIUS IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
     
    
-    spotifyFrameEl.attr("src", spotifyUrl + userSign.virgo);
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.sagittarius);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
+$("#capricorn").on("click", function(event) {
+    console.log(spotifyUrl + userSign.capricorn);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN CAPRICORN IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.capricorn);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
+$("#aquarius").on("click", function(event) {
+    console.log(spotifyUrl + userSign.aquarius);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN AQUARIUS IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.aquarius);
+    spotifyDivEl.append(spotifyFrameEl);
+
+});
+$("#pisces").on("click", function(event) {
+    console.log(spotifyUrl + userSign.pisces);
+    event.preventDefault();
+    console.log("DEBUG: THIS WILL SHOW WHEN PISCES IS CLICKED");
+    console.log("EXAMPLE OF BUTTON");    
+    
+   
+    spotifyFrameEl.attr("src", spotifyUrl + userSign.pisces);
     spotifyDivEl.append(spotifyFrameEl);
 
 });
 
+//calendar click functions
 
+
+$(".calendarNum").on("click", function() {
+    userNum = $(this).val();
+    console.log(userNum);
+    $(userNum).addClass("active");
+    
+
+
+});
+//event listener for the div to get the moment month and year?
+
+
+$(".prev").on("click", function(event) {
+    event.preventDefault();
+    console.log("Hello");
+    monthEl = moment().add(1,'month');
+
+    
+});
+$(".next").on("click", function(event) {
+    event.preventDefault();
+    console.log("World");
+    
+});
    
+
