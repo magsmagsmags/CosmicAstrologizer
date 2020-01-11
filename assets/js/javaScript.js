8aaabcb83fa674504099e72af16c5c2d641752d2
Luke2theDuke <57816716+Luke2theDuke@users.nopreply.github.com>
2020-01-11 11:48:15 -0600

    Making sure we are all in sync

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -357,9 +357,72 @@ $(document).ready(function () {
 
 
 
+/* MODAL FUNCTIONALITY SCRIPTS */
+class BulmaModal {
+    constructor(selector) {
+        this.elem = document.querySelector(selector)
+        this.close_data()
+    }
+
+    show() {
+        this.elem.classList.toggle('is-active')
+        this.on_show()
+    }
+
+    close() {
+        this.elem.classList.toggle('is-active')
+        this.on_close()
+    }
+
+    close_data() {
+        var modalClose = this.elem.querySelectorAll("[data-bulma-modal='close'], .modal-background")
+        var that = this
+        modalClose.forEach(function (e) {
+            e.addEventListener("click", function () {
+
+                that.elem.classList.toggle('is-active')
+
+                var event = new Event('modal:close')
+
+                that.elem.dispatchEvent(event);
+            })
+        })
+    }
+
+    on_show() {
+        var event = new Event('modal:show')
+
+        this.elem.dispatchEvent(event);
+    }
+
+    on_close() {
+        var event = new Event('modal:close')
+
+        this.elem.dispatchEvent(event);
+    }
+
+    addEventListener(event, callback) {
+        this.elem.addEventListener(event, callback)
+    }
+}
 
 
+/* START MODAL SCRIPTS */
+var mrBtn = document.querySelector("#modalBtn")
+var mrMdl = new BulmaModal("#mrModal")
 
+mrBtn.addEventListener("click", function () {
+    mrMdl.show()
+})
+
+mrMdl.addEventListener('modal:show', function () {
+    console.log("opened")
+})
+
+mrMdl.addEventListener("modal:close", function () {
+    console.log("closed")
+})
+/* END ARIES MODAL SCRIPTS */
 
 
 
